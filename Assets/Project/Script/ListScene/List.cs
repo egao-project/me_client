a0c362eb6b19495760f1bc2a83a8d4eed484c7e4
tom <oyamat@egao-inc.co.jp>
2018-01-26 22:01:09 +0900

    新規作成

---
+++ Assets/Project/Script/ListScene/List.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class List : MonoBehaviour {
+
+	[SerializeField] Image _initialImage;
+	[SerializeField] Text _titleText;
+	[SerializeField] Text _timeText;
+	[SerializeField] Text _fileDitailText;
+
+	Image InitialImage {
+		get{
+			 return _initialImage;
+		}
+		set{
+			_initialImage = value;
+		}
+	}
+
+	Text TimeText{
+		get{
+			return _titleText;
+		}
+		set{
+			_titleText = value;
+		}
+	}
+	Text TitleText{
+		get{
+			return _timeText;
+		}
+		set{
+			_timeText = value;
+		}
+	}
+	Text FileDitailText
+	{
+		get{
+			return _fileDitailText;
+		}
+		set{
+			_fileDitailText = value;
+		}
+	}
+}
