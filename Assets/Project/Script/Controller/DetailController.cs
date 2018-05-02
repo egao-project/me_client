0e5896b1cfe6e2b08d2655dec1cf9c77172e5af9
株式会社イーガオ <egao@kabushikikaishaigaonoMacBook-puro.local>
2018-05-02 22:50:51 +0900

    クライアントテスト実施（ただし画像読み込み重い）

--- Assets/Project/Script/Controller/DetailController.cs
+++ Assets/Project/Script/Controller/DetailController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using Kakera;
+using UnityEngine.SceneManagement;
+
 
 public class DetailController : BaseController {
 
@@ -17,34 +19,36 @@ public class DetailController : BaseController {
 	private string[] urls = new string[5];
 	private int[] positions = new int[5];
 	private int[] ids = new int[5];
-	private Hashtable master = new Hashtable();
+	private Picture[] master = new Picture[5];
 	private Frame frame = ListController.list [ListController.frame_idx];
 
 	// Use this for initialization
 	IEnumerator Start () {
 		base.Start ();
 
-		urls = frame.path_list.Split (',');
-		InitArrString(urls,frame.path_list);
-		InitArrInt(positions, frame.position_list);
-		InitArrInt (ids, frame.id_list);
+		if (frame.path_list != null && frame.path_list != "") {
+			urls = frame.path_list.Split (',');
+			InitArrString (urls, frame.path_list);
+			InitArrInt (positions, frame.position_list);
+			InitArrInt (ids, frame.id_list);
 
-		for (int i = 0; i < positions.Length; i++) {
-			Picture p = new Picture ();
-			p.position = positions [i];
-			p.id = ids [i];
-			p.image = urls [i];
-			master.Add (positions [i], p);
-		}
+			for (int i = 0; i < urls.Length; i++) {
+				Picture p = new Picture ();
+				p.position = positions [i];
+				p.id = ids [i];
+				p.image = urls [i];
+				master [p.position] = p;
+			}
 
-		int idx = 0;
-		foreach (string u in urls) {
-			WWW www = new WWW(u);
-			// 画像ダウンロード完了を待機
-			yield return www;
-			var texture = www.texture;
-			imageRenderer[positions[idx]].sprite = Sprite.Create(texture, new Rect(0, 0, texture.width, texture.height), Vector2.zero);
-			idx++;
+			int idx = 0;
+			foreach (string u in urls) {
+				WWW www = new WWW (u);
+				// 画像ダウンロード完了を待機
+				yield return www;
+				var texture = www.texture;
+				imageRenderer [positions [idx]].sprite = Sprite.Create (texture, new Rect (0, 0, texture.width, texture.height), Vector2.zero);
+				idx++;
+			}
 		}
 	}
 
@@ -78,17 +82,35 @@ public class DetailController : BaseController {
 		{
 			Debug.LogError("Failed to load texture url:" + url);
 		}
+		Debug.LogError ("1");
 		output.sprite = Sprite.Create(texture, new Rect(0, 0, texture.width, texture.height), Vector2.zero);
 		//output.material.mainTexture = texture;
+		Debug.LogError ("2");
 
-		Picture p = (Picture) master [index];
+		Picture p = master [index];
+		Debug.LogError ("3");
 		HttpConector http = new HttpConector ();
-		http.Delete (Const.PICTURE_DELETE_URL, p.id.ToString ());
-		HttpItem r = http.PostImage (url, frame.id, p.position);
+		Debug.LogError ("4");
+		if (p != null) {
+			Debug.LogError ("5");
+			http.Delete (Const.PICTURE_DELETE_URL, p.id.ToString ());
+		}
+		Debug.LogError ("6");
+
+		HttpItem r = http.PostImage (path, frame.id, index);
+		Debug.LogError ("7");
+
 		Picture pp = JsonUtility.FromJson<Picture> (r.body);
+		Debug.LogError ("8");
+
 		p.id = pp.id;
 		master [index] = p;
 
+	}
+
+	public void PushCommitButton()
+	{
+		SceneManager.LoadScene ("ListScene"); 
 	}
 
 	private void InitArrString(string[] arr,string list)
