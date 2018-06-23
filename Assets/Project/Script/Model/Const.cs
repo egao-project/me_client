7c1158a30e2021c7c9071bb9c811fd7e344b56db
株式会社イーガオ <egao@kabushikikaishaigaonoMacBook-puro.local>
2018-06-23 21:08:16 +0900

    aws対応

--- Assets/Project/Script/Model/Const.cs
+++ Assets/Project/Script/Model/Const.cs
@@ -3,10 +3,10 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Const {
-	public const string LOGIN_URL="https://egao-me-server.herokuapp.com/login/";
-	public const string FRAME_URL="https://egao-me-server.herokuapp.com/api/frames/show/";
-	public const string FRAME_ADD_URL="https://egao-me-server.herokuapp.com/api/frames/add/";
-	public const string IMAGE_POST_URL="https://egao-me-server.herokuapp.com/api/pictures/upload/";
-	public const string PICTURE_DELETE_URL="https://egao-me-server.herokuapp.com/api/pictures/delete/";
-	public const string VIEW_URL="https://egao-me-server.herokuapp.com/me_view/";
+	public const string LOGIN_URL="http://ec2-13-230-96-6.ap-northeast-1.compute.amazonaws.com/login/";
+	public const string FRAME_URL="http://ec2-13-230-96-6.ap-northeast-1.compute.amazonaws.com/api/frames/show/";
+	public const string FRAME_ADD_URL="http://ec2-13-230-96-6.ap-northeast-1.compute.amazonaws.com/api/frames/add/";
+	public const string IMAGE_POST_URL="http://ec2-13-230-96-6.ap-northeast-1.compute.amazonaws.com/api/pictures/upload/";
+	public const string PICTURE_DELETE_URL="http://ec2-13-230-96-6.ap-northeast-1.compute.amazonaws.com/api/pictures/delete/";
+	public const string VIEW_URL="http://ec2-13-230-96-6.ap-northeast-1.compute.amazonaws.com/me_view/";
 }
