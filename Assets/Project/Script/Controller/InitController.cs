a71139a8a888c8f2f2fb845b53d01dd21bcfab72
tom <tomokinooniku1@hotmail.com>
2018-07-05 12:35:48 +0900

    【暗号化未実装】PlayerPrefsでTokenキーを保持

--- Assets/Project/Script/Controller/InitController.cs
+++ Assets/Project/Script/Controller/InitController.cs
@@ -29,6 +29,14 @@ public class InitController : BaseController {
 	public void Start () {
 		ViewLoading ();
 
+		//初期起動状態かローカルに保存しているTokenStringを確認
+		if (PlayerPrefs.GetString ("TokenKey") == null) {
+			//signup処理:TODO
+		} else {
+			//トークンキーが期限切れ及び一致しない場合の処理
+
+		}
+
 		Invoke("ViewLogin", 1.0f);
 	}
 	
@@ -53,9 +61,17 @@ public class InitController : BaseController {
 
 		if (r.code == 200) {
 			User u = JsonUtility.FromJson<User> (r.body);
+			//ユーザ情報を保持
 			u.username = useridInput.text;
 			u.password = passInput.text;
 			BaseController.SetUser(u);
+
+			//Tokenキーを保存
+			PlayerPrefs.SetString("TokenKey", u.token);
+
+
+			Debug.Log (PlayerPrefs.GetString ("TokenKey"));
+
 			SceneManager.LoadScene ("ListScene"); 
 		} else {
 			nextCanvas.SetActive (false);
@@ -93,6 +109,11 @@ public class InitController : BaseController {
 		loadingCanvas.SetActive (false);
 		singinCanvas.SetActive (false);
 		loginCanvas.SetActive (true);
+	}
+	private void signup () {
+		loadingCanvas.SetActive (false);
+		singinCanvas.SetActive (false);
+		loginCanvas.SetActive (false);
 	}
 
 }
