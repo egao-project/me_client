105ab0891d2aee1b3f30f7ff3a58967b4bfa6044
tom <tomokinooniku1@hotmail.com>
2018-07-20 18:45:36 +0900

    URL変更

--- Assets/Project/Script/Model/Const.cs
+++ Assets/Project/Script/Model/Const.cs
@@ -3,11 +3,11 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Const {
-	public const string LOGIN_URL="http://ec2-13-230-96-6.ap-northeast-1.compute.amazonaws.com/login/";
-	public const string FRAME_URL="http://ec2-13-230-96-6.ap-northeast-1.compute.amazonaws.com/api/frames/show/";
-	public const string FRAME_ADD_URL="http://ec2-13-230-96-6.ap-northeast-1.compute.amazonaws.com/api/frames/add/";
-	public const string IMAGE_POST_URL="http://ec2-13-230-96-6.ap-northeast-1.compute.amazonaws.com/api/pictures/upload/";
-	public const string PICTURE_DELETE_URL="http://ec2-13-230-96-6.ap-northeast-1.compute.amazonaws.com/api/pictures/delete/";
-	public const string VIEW_URL="http://ec2-13-230-96-6.ap-northeast-1.compute.amazonaws.com/me_view/";
-	public const string REGISTER="http://ec2-13-230-96-6.ap-northeast-1.compute.amazonaws.com/auth/register/";
+	public const string LOGIN_URL="https://www.egao-me.com/login/";
+	public const string FRAME_URL="https://www.egao-me.com/api/frames/show/";
+	public const string FRAME_ADD_URL="https://www.egao-me.com/api/frames/add/";
+	public const string FRAME_ADD_TITLE="https://www.egao-me.com/api/frames/add_title/";
+	public const string IMAGE_POST_URL="https://www.egao-me.com/api/pictures/upload/";
+	public const string PICTURE_DELETE_URL="https://www.egao-me.com/api/pictures/delete/";
+	public const string VIEW_URL="https://www.egao-me.com/me_view/";
 }
